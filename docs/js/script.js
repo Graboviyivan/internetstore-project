743f6b3b08f31aca460257dbabfdacb89de2ef34
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2026-07-01 21:46:13 +0300

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,17 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const payBtn = document.getElementById('pay-btn');
-  const backdrop = document.querySelector('.backdrop');
-  const payModal = document.querySelector('.pay');
+const payBtn = document.getElementById("pay-btn");
+const backdrop = document.querySelector(".backdrop");
+const payModal = document.querySelector(".pay");
 
-  // Функція для відкриття модалки
-  payBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    backdrop.classList.add('is-open');
-    payModal.classList.add('is-open');
-  });
+payBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  backdrop.classList.add("is-open");
+  payModal.classList.add("is-open");
+});
 
-  backdrop.addEventListener('click', () => {
-    backdrop.classList.remove('is-open');
-    payModal.classList.remove('is-open');
-  });
+backdrop.addEventListener("click", () => {
+  backdrop.classList.remove("is-open");
+  payModal.classList.remove("is-open");
 });
